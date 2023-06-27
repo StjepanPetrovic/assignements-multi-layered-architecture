34a1bc20f1bca93bf3d726ebb611e46054852d09
Stjepan Petrović <stjepanpetrovic11@gmail.com>
2023-06-27 15:21:14 +0200

    Add AssignementServices and  AssignementStatus Services classes in Business logic layer

---
+++ Business logic layer/AssignementServices.cs
@@ -0,0 +1,49 @@
+﻿using Data_access_layer.Repositories;
+using Entity_layer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business_logic_layer
+{
+    public class AssignementServices
+    {
+        public List<Assignment> getAssignments()
+        {
+            using (var repo = new AssignmentRepository())
+            {
+                List<Assignment> assignments = repo.GetAll().ToList();
+
+                return assignments;
+            }
+        }
+
+        public bool addAssignemnt(Assignment assignment)
+        {
+            bool isSuccessful = false;
+
+            using (var repo = new AssignmentRepository())
+            {
+                int affectedRows = repo.Add(assignment);
+                isSuccessful = affectedRows > 0;
+            }
+
+            return isSuccessful;
+        }
+
+        public bool removeAssignment(Assignment assignment)
+        {
+            bool isSuccessful = false;
+
+            using (var repo = new AssignmentRepository())
+            {
+                int affectedRows = repo.Remove(assignment);
+                isSuccessful = affectedRows > 0;
+            }
+
+            return isSuccessful;
+        }
+    }
+}
---
+++ Business logic layer/AssignementStatusServices.cs
@@ -0,0 +1,24 @@
+﻿using Data_access_layer;
+using Data_access_layer.Repositories;
+using Entity_layer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business_logic_layer
+{
+    public class AssignementStatusServices
+    {
+        public List<AssignmentStatus> getAssignments()
+        {
+            using (var repo = new AssignmentStatusRepository())
+            {
+                List<AssignmentStatus> assignmentsStatus = repo.GetAll().ToList();
+
+                return assignmentsStatus;
+            }
+        }
+    }
+}
