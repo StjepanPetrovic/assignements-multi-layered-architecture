d67f4dd5fb0eeab07417849816a38b0693020478
Stjepan Petrović <stjepanpetrovic11@gmail.com>
2023-06-27 11:46:32 +0200

    Entity classes cutted from Data layer to Entites Layer

--- Entity layer/Entities/Assignment.cs
+++ Entity layer/Entities/Assignment.cs
@@ -4,7 +4,6 @@ namespace Data_access_layer
     using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
     using System.ComponentModel.DataAnnotations.Schema;
-    using System.Data.Entity.Spatial;
 
     public partial class Assignment
     {
