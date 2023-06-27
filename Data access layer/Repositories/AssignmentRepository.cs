cfec390a26d56bf95c8aed0b05997f74b05db5b6
Stjepan Petrović <stjepanpetrovic11@gmail.com>
2023-06-27 12:23:02 +0200

    Add repositories classes which inherits Repository class for Assignements and Assignement statuses

---
+++ Data access layer/Repositories/AssignmentRepository.cs
@@ -0,0 +1,22 @@
+﻿using Entity_layer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_access_layer.Repositories
+{
+    public class AssignmentRepository : Repository<Assignment>
+    {
+        public AssignmentRepository() : base(new AssignementsModel())
+        {
+
+        }
+
+        public override int Update(Assignment entity, bool saveChanges = true)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Data access layer/Repositories/AssignmentStatusRepository.cs
@@ -0,0 +1,21 @@
+﻿using Entity_layer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_access_layer.Repositories
+{
+    public class AssignmentStatusRepository : Repository<AssignmentStatus>
+    {
+        public AssignmentStatusRepository() : base(new AssignementsModel())
+        {
+
+        }
+        public override int Update(AssignmentStatus entity, bool saveChanges = true)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
